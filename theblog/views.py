c343498bc6f545c8c59fadc66ca515b4edcc3c56
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-15 15:03:54 +0000

    Create a list of all blog posts on the Home page and its view and URL using ListView

--- theblog/views.py
+++ theblog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from .models import Post
 
 
 # def home(request):
 #     return render(request, 'home.html', {})
 
 
-# Django class-based view
+# Django class-based view for Home page
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
