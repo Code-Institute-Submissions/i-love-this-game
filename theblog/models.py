e2ff8735dff470e0fd320856790bb0ce6bd95c87
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-15 14:27:21 +0000

    Create Django class-based view for the Home page

--- theblog/models.py
+++ theblog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# Post model
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- theblog/views.py
+++ theblog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-def home(request):
-    return render(request, 'home.html', {})
+
+# def home(request):
+#     return render(request, 'home.html', {})
+
+
+# Django class-based view
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
