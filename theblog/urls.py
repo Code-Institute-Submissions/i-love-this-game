5273c0834e81a60e3590434cdf924e8ae6ff4a36
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-17 15:36:06 +0000

    Create a delete post page and post-deleting functionality using DeleteView and reverse_lazy redirection

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 # URLs for each HTML view
 urlpatterns = [
@@ -11,4 +11,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(
     ), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(
+    ), name='delete_post'),
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 
 # def home(request):
@@ -36,3 +37,10 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'update_post.html'
     # fields = ['title', 'title_tag', 'body']
+
+
+# Django class-based view for delete post page
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
