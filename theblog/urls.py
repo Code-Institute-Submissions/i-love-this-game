ed82dcff3224fb50dc69375b71d7ccf42a525a9a
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-20 12:19:25 +0000

    Modify the forms.py file so that a category can be designated for each new blog post created

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
 
 # URLs for each HTML view
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('article/<int:pk>', ArticleDetailView.as_view(
     ), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(
     ), name='update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(
