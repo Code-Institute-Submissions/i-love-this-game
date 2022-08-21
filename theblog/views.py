fe9507d9f36a79dbfe791244a718d6d9943aeec2
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-21 20:11:08 +0000

    Create an unlike button by looking in the table to see if a like for a post for some user exists and remove it if it does

--- theblog/views.py
+++ theblog/views.py
@@ -9,9 +9,16 @@ from django.urls import reverse_lazy, reverse
 # def home(request):
 #     return render(request, 'home.html', {})
 
+# Django function-based view for the like button
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -53,10 +60,14 @@ class ArticleDetailView(DetailView):
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(
             *args, **kwargs)
-        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
-        total_likes = stuff.total_likes()
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = post.total_likes()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 
