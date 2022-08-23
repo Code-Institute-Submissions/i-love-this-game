582ceecc0cf6849dd44350e6f818658988c465b5
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-23 09:23:29 +0000

    Create a page to edit a user's profile using the Django User Authentication system

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 
 
 # URLs for each HTML view in the members app
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 ]
