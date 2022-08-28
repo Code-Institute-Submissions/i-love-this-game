706e87f435281d53ad571d411b4cba91095f72d9
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-08-28 13:37:08 +0000

    Change the body field on the Post model to prevent users from submitting empty posts

--- theblog/models.py
+++ theblog/models.py
@@ -43,7 +43,8 @@ class Post(models.Model):
         'image', null=True, blank=True, folder="media/")
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = RichTextField(blank=True, null=True)
+    body = RichTextField(
+        blank=False, null=False, default='Write your post here.')
     # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='players')
